bf7a404547b43a8a773b3fef258b671b5273f8a6
Etienne Garnier <garnier.etienne@gmail.com>
2013-09-10 21:27:24 +0200

    format output in chef formatter

--- lib/appd-chef-formatter.rb
+++ lib/appd-chef-formatter.rb
@@ -5,12 +5,15 @@ class Chef
       cli_name(:appd)
 
       def resource_action_start(resource, action, notification_type = nil, notifier = nil)
-        print "* #{action} #{resource.name} #{resource.resource_name}"
+        print "* #{action if action != :execute} #{resource.name} #{resource.resource_name}" if action != :nothing
       end
 
-      def resource_completed(resource)
-        puts "OK!"
-        super
+      def resource_up_to_date(resource, action)
+        puts "(nothing to do)"
+      end
+
+      def resource_updated(resource, action)
+        puts "(#{resource} #{action})"
       end
     end
   end
