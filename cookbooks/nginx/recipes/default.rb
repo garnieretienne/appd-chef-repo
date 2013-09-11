6a9799e51a1dee4c5c1306947f7828b42e2d55cf
Etienne Garnier <garnier.etienne@gmail.com>
2013-09-11 19:03:35 +0200

    enable nginx service

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -3,6 +3,12 @@ package 'nginx' do
   action :install
 end
 
+# Ensure nginx service is configured to be started at boot
 service 'nginx' do
   action :enable
+end
+
+# Ensure 'nginx' service is running
+service 'nginx' do
+  action :start
 end
