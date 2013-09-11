c8894f49a623f4ed9ae79fcf14a48e736f984c5e
Etienne Garnier <garnier.etienne@gmail.com>
2013-09-11 19:16:51 +0200

    add gitreceive recipe

---
+++ cookbooks/gitreceive/recipes/default.rb
@@ -0,0 +1,22 @@
+# Ensure `git` package is installed
+package 'git' do
+  action :install
+end
+
+# Create the git user
+user "deploy" do
+  username deploy
+  comment "GIT receive user"
+  shell "/bin/false"
+  home "/srv/git/deploy"
+  supports manage_home: true
+  action :create
+end
+
+# Create a bin dir in the git user home
+directory "/srv/git/deploy/bin" do
+  owner "deploy"
+  group "deploy"
+  mode 0755
+  action :create
+end
